b6a6316fce95c5bcc08ab56f8806fc5c958d76da
harsh <66548698+harsssh@users.noreply.github.com>
2024-05-22 11:13:08 +0900

    done?

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,37 @@
+'use client'
+
+import { useEffect, useRef, useState } from 'react'
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  const [topOffset, setTopOffset] = useState(0);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver(entries => {
+      for (let entry of entries) {
+        setTopOffset(entry.target.clientHeight);
+      }
+    });
+
+    if (ref.current) {
+      resizeObserver.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        resizeObserver.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+  return (
+    <>
+      <div ref={ref} className="bg-sky-500 sticky top-0">
+        <textarea />
+      </div>
+      <div className="bg-lime-500 h-screen" />
+      <div className="bg-amber-500 h-20 sticky" style={{ top: `${topOffset}px` }} />
+      <div className="bg-stone-500 h-screen" />
+    </>
+  );
 }
